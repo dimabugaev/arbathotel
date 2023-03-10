2fce6cb30458fed35848eb15cd64e6494f76c9eb
Dmitry Bugaev <bugaefff@gmail.com>
2023-03-10 23:12:03 +0300

    correct model + trigers

---
+++ 3_databasescripts/populate_tables.sql
@@ -0,0 +1,22 @@
+INSERT INTO operate.hotels (hotel_name) VALUES('Общее');
+INSERT INTO operate.hotels (hotel_name) VALUES('Академическая');
+INSERT INTO operate.hotels (hotel_name) VALUES('Авиамоторная');
+INSERT INTO operate.hotels (hotel_name) VALUES('Ботанический');
+INSERT INTO operate.hotels (hotel_name) VALUES('Измайловский');
+INSERT INTO operate.hotels (hotel_name) VALUES('Каширский');
+INSERT INTO operate.hotels (hotel_name) VALUES('Нахимовский');
+INSERT INTO operate.hotels (hotel_name) VALUES('Покровский');
+INSERT INTO operate.hotels (hotel_name) VALUES('Семеновский');
+INSERT INTO operate.hotels (hotel_name) VALUES('Соколиная Гора');
+INSERT INTO operate.hotels (hotel_name) VALUES('Таганская');
+INSERT INTO operate.hotels (hotel_name) VALUES('Хамовники');
+INSERT INTO operate.hotels (hotel_name) VALUES('Чистопрудный');
+INSERT INTO operate.hotels (hotel_name) VALUES('Склад');
+INSERT INTO operate.hotels (hotel_name) VALUES('Токмаков');
+
+
+select *
+from operate.hotels h;
+
+select *
+from operate.report_items ri;

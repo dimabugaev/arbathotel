ff00f175b25e97df5fbcd84811ac9037a2b56e61
Dmitry Bugaev <bugaefff@gmail.com>
2023-03-15 12:17:14 +0300

    lambda powertools was add

--- 2_infrastructure/src/employees_reports_data.py
+++ 2_infrastructure/src/employees_reports_data.py
@@ -1,18 +1,9 @@
-# import json
-
-# def lambda_handler(event, context):
-#     print("Hello from Lambda!")
-
-
-#     responseObj = {}
-#     responseObj["statusCode"] = 200
-#     responseObj["headers"] = {}
-#     responseObj["headers"]["Content-Type"] = 'application/json'
-#     responseObj['body'] = json.dumps('Hello from Lambda!')
-#     return responseObj
-
 import psycopg2
 import json
+import re
+#from aws_lambda_powertools import Logger, Tracer
+from aws_lambda_powertools.event_handler import APIGatewayHttpResolver
+
 
 endpoint = 'dev.coxuw68luhb8.eu-central-1.rds.amazonaws.com'
 username = 'dev'
@@ -20,8 +11,43 @@ password = 'rootroot'
 database_name = 'dev_arbathotel'
 
 connection = psycopg2.connect(host=endpoint, database=database_name, user=username, password=password)
+#tracer = Tracer()
+#logger = Logger()
+app = APIGatewayHttpResolver()
 
-def lambda_handler(event, context):
+def get_response(body: dict = {}) -> dict:
+    response_obj = {}
+    response_obj["statusCode"] = 200
+    response_obj["headers"] = {}
+    response_obj["headers"]["Content-Type"] = 'application/json'
+    response_obj['body'] = json.dumps(body)
+
+    return response_obj
+
+@app.get("/string")
+def get_report_strings():
+    print(app.current_event.query_string_parameters)
+    source_id = app.current_event.query_string_parameters.get("source_id")
+    date_start = app.current_event.query_string_parameters.get("date_start")
+    date_end = app.current_event.query_string_parameters.get("date_end")
+    mode = app.current_event.query_string_parameters.get("mode")
+    
+    if mode is None: 
+        mode = 0
+    #else 
+ 
+
+@app.post("/string")
+def put_operate_report_strings():
+    print(app.current_event.query_string_parameters)
+
+@app.get("/dict")
+def get_hotels_and_report_ivents() -> dict:
+    source_id = app.current_event.query_string_parameters.get("source_id")
+
+    if re.match('\S+', source_id) is None: # bad string
+        return get_response({'FormatError': source_id})
+
     cursor = connection.cursor()
     
     cursor.execute("""SELECT 
@@ -35,24 +61,30 @@ def lambda_handler(event, context):
     bodyDict = {}
     bodyDict["hotels"] = cursor.fetchall()
     
-    cursor.execute("""SELECT 
-                        id, 
-                        item_name 
-                      FROM 
-                        operate.report_items""")
+    cursor.execute("""with approve_items as (
+                        select
+                          ris.report_item_id as id 
+                        from 
+                          operate.report_items_setings ris
+                        where 
+                          ris.source_id in 
+                            (select id from operate.sources so where so.source_external_key = %(source_key)s)
+                          and ris.view_permission = TRUE
+                      )
+                      select 
+                        ri.id,
+                        ri.item_name 
+                      from 
+                        operate.report_items ri inner join
+                          approve_items ap on (ri.id = ap.id)""", {'source_key': source_id})
                         
     bodyDict["report_items"] = cursor.fetchall()
     
-    responseObj = {}
-    responseObj["statusCode"] = 200
-    responseObj["headers"] = {}
-    responseObj["headers"]["Content-Type"] = 'application/json'
-    #responseObj['body'] = {}
-    #responseObj['body']['hotels'] = json.dumps(rows)
-    responseObj['body'] = json.dumps(bodyDict)
+    return get_response(bodyDict)
 
-    print(responseObj)
+@app.post("/close")
+def current_string_to_histirical():
+    print(app.current_event.query_string_parameters)
 
-    return responseObj
-
-#handler()
+def lambda_handler(event, context):
+    return app.resolve(event, context)
--- 3_databasescripts/populate_tables.sql
+++ 3_databasescripts/populate_tables.sql
@@ -19,13 +19,49 @@ INSERT INTO operate.report_items (item_name) VALUES('Зарплата');
 INSERT INTO operate.report_items (item_name) VALUES('Хозрасходы');
 INSERT INTO operate.report_items (item_name) VALUES('Прочее');
 
+INSERT INTO operate.employees (last_name, first_name, name_in_db) VALUES('Авдеева','Ольга','Авдеева');
+INSERT INTO operate.employees (last_name, first_name, name_in_db) VALUES('Быкова','Ирина','Быкова');
+INSERT INTO operate.employees (last_name, first_name, name_in_db) VALUES('Пульчев','Денис','Пульчев');
+INSERT INTO operate.employees (last_name, first_name, name_in_db) VALUES('Салдаев','Алексей','Салдаев');
 
+INSERT INTO operate.sources (source_name, source_external_key, source_type) 
+VALUES('Отчет Викулин Тест 1','1VSOfTBULFm2L2AgZ9-HLRO5QPivhpSAqpyd9jAz2KG8',1);
+
+
+with approve_items as (
+	select
+		ris.report_item_id as id 
+	from 
+		operate.report_items_setings ris
+	where 
+		ris.source_id in 
+			(select id from operate.sources so where so.source_external_key = '1VSOfTBULFm2L2AgZ9-HLRO5QPivhpSAqpyd9jAz2KG8')
+		and ris.view_permission = TRUE
+)
+select 
+	ri.id,
+	ri.item_name 
+from 
+	operate.report_items ri inner join
+		approve_items ap on (ri.id = ap.id);
+	
 
 select *
 from operate.hotels h;
 
 select *
 from operate.report_items ri;
+
+select *
+from operate.employees ri;
+
+select *
+from operate.report_items_setings ris; 
+
+select *
+from operate.sources s;
+
+
 
 update operate.hotels 
 set hotel_name = 'PUTIN KHUILO'

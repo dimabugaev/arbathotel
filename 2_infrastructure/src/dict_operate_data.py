7e1a838b990bf93da07ff964b9b85d6bfe80f61a
Dmitry Bugaev <bugaefff@gmail.com>
2023-03-30 22:56:19 +0300

    check errors 2

--- 2_infrastructure/src/dict_operate_data.py
+++ 2_infrastructure/src/dict_operate_data.py
@@ -3,6 +3,7 @@ import json
 import re
 #from aws_lambda_powertools import Logger, Tracer
 from aws_lambda_powertools.event_handler import APIGatewayHttpResolver
+from typing import Any
 
 import boto3
 from datetime import date, timedelta, datetime
@@ -44,7 +45,7 @@ def num_to_query_substr(id: any, result_if_null = "NULL") -> str:
           result = id
     return result
 
-def get_sources() -> list[tuple]:
+def get_sources() -> list[tuple[Any, ...]]:
     cursor = connection.cursor()
     
     cursor.execute("""select 
@@ -58,7 +59,7 @@ def get_sources() -> list[tuple]:
     
     return cursor.fetchall()
                             
-def get_hotels() -> list[tuple]:
+def get_hotels() -> list[tuple[Any, ...]]:
     cursor = connection.cursor()
     
     cursor.execute("""select 
@@ -69,7 +70,7 @@ def get_hotels() -> list[tuple]:
     
     return cursor.fetchall()
 
-def get_employees() -> list[tuple]:
+def get_employees() -> list[tuple[Any, ...]]:
     cursor = connection.cursor()
     
     cursor.execute("""select 
@@ -82,7 +83,7 @@ def get_employees() -> list[tuple]:
     
     return cursor.fetchall()
 
-def get_report_items() -> list[tuple]:
+def get_report_items() -> list[tuple[Any, ...]]:
     cursor = connection.cursor()
     
     cursor.execute("""select 
@@ -100,7 +101,7 @@ def get_report_items() -> list[tuple]:
     
     return cursor.fetchall()
 
-def get_report_settings(source_id : str) -> list[tuple]:
+def get_report_settings(source_id : str) -> list[tuple[Any, ...]]:
     if re.match('\S+', source_id) is None: # bad string
         return get_response({'FormatError': source_id})
     
@@ -131,7 +132,7 @@ def check_sources_row(datastr : tuple) -> bool:
 
     return True
 
-def put_sources(datastrings: list[tuple]):
+def put_sources(datastrings: list[tuple[Any, ...]]):
 
     if len(datastrings) > 0:
         cursor = connection.cursor()

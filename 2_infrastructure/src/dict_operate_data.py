503621f732a78b79e1fb1f7ad33ed96b4079fa76
Dmitry Bugaev <bugaefff@gmail.com>
2023-04-06 20:50:17 +0300

    post dict

--- 2_infrastructure/src/dict_operate_data.py
+++ 2_infrastructure/src/dict_operate_data.py
@@ -97,7 +97,9 @@ def get_report_items() -> list:
                       from 
                         operate.report_items ri
                         left join operate.employees em on ri.empl_id = em.id
-                        left join operate.sources so on ri.source_id = so.id""")
+                        left join operate.sources so on ri.source_id = so.id
+                      order by
+                        ri.order_count""")
     
     return cursor.fetchall()
 
@@ -116,7 +118,7 @@ def get_report_settings(source_id : str) -> list:
                             limit 1
                         )
                         select
-                            so.id,
+                            rs.source_id,
                             so.source_name,  
                             rs.report_item_id,
                             ri.item_name,
@@ -128,9 +130,6 @@ def get_report_settings(source_id : str) -> list:
     
     return cursor.fetchall()
 
-def check_sources_row(datastr : tuple) -> bool:
-
-    return True
 
 def put_sources(datastrings: list):
 
@@ -141,6 +140,7 @@ def put_sources(datastrings: list):
           
           list_of_args = []
           for newrow in datastrings:
+
               if (len(str(newrow[0])) == 0 
                   and len(str(newrow[1])) == 0 
                   and len(str(newrow[2])) == 0 
@@ -157,39 +157,294 @@ def put_sources(datastrings: list):
 
           if len(list_of_args) > 0:
             cursor.execute("""DROP TABLE IF EXISTS temp_source_table_update""")
-            cursor.execute("""CREATE TEMP TABLE temp_source_table AS SELECT * FROM operate.sources WHERE false""")
+            cursor.execute("""CREATE TEMP TABLE temp_source_table_update AS SELECT * FROM operate.sources WHERE false""")
 
             args_str = ','.join(list_of_args)
-            cursor.execute("""INSERT INTO temp_source_table
+            cursor.execute("""INSERT INTO temp_source_table_update
                                 (id, source_name, source_type, source_external_key, source_income_debt)
                               VALUES """ + args_str)
             
+
             cursor.execute("""
-                MERGE INTO operate.sources s
-                    USING temp_source_table_update u ON u.id = s.id
-                    WHEN MATCHED AND EXISTS (
-                                        SELECT s.source_name, s.source_type, s.source_external_key, s.source_income_debt
-                                        EXCEPT
-                                        SELECT u.source_name, u.source_type, u.source_external_key, u.source_income_debt
-                                    )
-                    THEN
-                        UPDATE SET  s.source_name = u.source_name,
-                                    s.source_type = u.source_type,
-                                    s.source_external_key = u.source_external_key,
-                                    s.source_income_debt = u.source_income_debt
-                    WHEN NOT MATCHED BY TARGET AND u.id is NULL
-                    THEN
-                        INSERT (source_name, source_type, source_external_key, source_income_debt)
-                        VALUES (u.source_name, u.source_type, u.source_external_key, u.source_income_debt)
-                                
-            """)
+                    
+                    UPDATE operate.sources t
+                        SET source_name = u.source_name,
+                            source_type = u.source_type,
+                            source_external_key = u.source_external_key,
+                            source_income_debt = u.source_income_debt
+                    FROM operate.sources s
+                        INNER JOIN temp_source_table_update u ON u.id = s.id
+                    WHERE EXISTS (
+                        SELECT s.source_name, s.source_type, s.source_external_key, s.source_income_debt
+                        EXCEPT
+                        SELECT u.source_name, u.source_type, u.source_external_key, u.source_income_debt
+                        ) and t.id = s.id;
+
+                    INSERT INTO operate.sources (source_name, source_type, source_external_key, source_income_debt)
+                    SELECT     
+                        source_name, 
+                        source_type, 
+                        source_external_key, 
+                        source_income_debt
+                    FROM temp_source_table_update
+                    WHERE
+                        id is NULL;
+                    """)
 
             connection.commit()
         except Exception as ex:
           connection.rollback()
           raise ex
 
-        
+
+def put_hotels(datastrings: list):
+
+    if len(datastrings) > 0:
+        cursor = connection.cursor()
+
+        try:
+          
+          list_of_args = []
+          for newrow in datastrings:
+
+              if (len(str(newrow[0])) == 0 
+                  and len(str(newrow[1])) == 0):
+                  continue
+              
+
+              list_of_args.append("({}, '{}')"
+                  .format(num_to_query_substr(newrow[0]), #id
+                  newrow[1]))                             #hotel_name
+                  
+
+          if len(list_of_args) > 0:
+            cursor.execute("""DROP TABLE IF EXISTS temp_hotels_table_update""")
+            cursor.execute("""CREATE TEMP TABLE temp_hotels_table_update AS SELECT * FROM operate.hotels WHERE false""")
+
+            args_str = ','.join(list_of_args)
+            cursor.execute("""INSERT INTO temp_hotels_table_update
+                                (id, hotel_name)
+                              VALUES """ + args_str)
+            
+
+            cursor.execute("""                    
+                    UPDATE operate.hotels t
+                        SET hotel_name = u.hotel_name
+                    FROM operate.hotels s
+                        INNER JOIN temp_hotels_table_update u ON u.id = s.id
+                    WHERE EXISTS (
+                        SELECT s.hotel_name
+                        EXCEPT
+                        SELECT u.hotel_name
+                        ) and t.id = s.id;
+
+                    INSERT INTO operate.hotels (hotel_name)
+                    SELECT     
+                        hotel_name
+                    FROM temp_hotels_table_update
+                    WHERE
+                        id is NULL;
+                    """)
+
+            connection.commit()
+        except Exception as ex:
+          connection.rollback()
+          raise ex        
+
+
+def put_employees(datastrings: list):
+
+    if len(datastrings) > 0:
+        cursor = connection.cursor()
+
+        try:
+          
+          
+          list_of_args = []
+          for newrow in datastrings:
+
+              if (len(str(newrow[0])) == 0 
+                  and len(str(newrow[1])) == 0 
+                  and len(str(newrow[2])) == 0 
+                  and len(str(newrow[3])) == 0):
+                  continue
+              
+              list_of_args.append("({}, '{}', '{}', '{}')"
+                  .format(num_to_query_substr(newrow[0]), #id
+                  newrow[1],                              #last_name
+                  newrow[2],                              #first_name
+                  newrow[3]))                             #name_in_db
+                  
+
+          if len(list_of_args) > 0:
+            cursor.execute("""DROP TABLE IF EXISTS temp_employees_table_update""")
+            cursor.execute("""CREATE TEMP TABLE temp_employees_table_update AS SELECT * FROM operate.employees WHERE false""")
+
+            args_str = ','.join(list_of_args)
+            cursor.execute("""INSERT INTO temp_employees_table_update
+                                (id, last_name, first_name, name_in_db)
+                              VALUES """ + args_str)
+            
+
+            cursor.execute("""
+                    
+                    UPDATE operate.employees t
+                        SET last_name = u.last_name,
+                            first_name = u.first_name,
+                            name_in_db = u.name_in_db
+                    FROM operate.employees s
+                        INNER JOIN temp_employees_table_update u ON u.id = s.id
+                    WHERE EXISTS (
+                        SELECT s.last_name, s.first_name, s.name_in_db
+                        EXCEPT
+                        SELECT u.last_name, u.first_name, u.name_in_db
+                        ) and t.id = s.id;
+
+                    INSERT INTO operate.employees (last_name, first_name, name_in_db)
+                    SELECT     
+                        last_name, 
+                        first_name, 
+                        name_in_db
+                    FROM temp_employees_table_update
+                    WHERE
+                        id is NULL;
+                    """)
+
+            connection.commit()
+        except Exception as ex:
+          connection.rollback()
+          raise ex
+
+
+def put_report_items(datastrings: list):
+
+    if len(datastrings) > 0:
+        cursor = connection.cursor()
+
+        try:
+          
+          list_of_args = []
+          for newrow in datastrings:
+              
+                        # ri.id,
+                        # ri.item_name,
+                        # ri.order_count, 
+                        # ri.empl_id,
+                        # em.name_in_db,
+                        # ri.source_id,
+                        # so.source_name 
+
+              if (len(str(newrow[0])) == 0 
+                  and len(str(newrow[1])) == 0 
+                  and len(str(newrow[2])) == 0 
+                  and len(str(newrow[3])) == 0 
+                  and len(str(newrow[5])) == 0):
+                  continue
+              
+              list_of_args.append("({}, '{}', {}, {}, {})"
+                  .format(num_to_query_substr(newrow[0]), #id
+                  newrow[1],                              #item_name
+                  num_to_query_substr(newrow[2]),         #order_count
+                  num_to_query_substr(newrow[3]),         #empl_id
+                  num_to_query_substr(newrow[5])))        #source_id
+
+          if len(list_of_args) > 0:
+            cursor.execute("""DROP TABLE IF EXISTS temp_items_table_update""")
+            cursor.execute("""CREATE TEMP TABLE temp_items_table_update AS SELECT * FROM operate.report_items WHERE false""")
+
+            args_str = ','.join(list_of_args)
+            cursor.execute("""INSERT INTO temp_items_table_update
+                                (id, item_name, order_count, empl_id, source_id)
+                              VALUES """ + args_str)
+            
+
+            cursor.execute("""
+                    
+                    UPDATE operate.report_items t
+                        SET item_name = u.item_name,
+                            order_count = u.order_count,
+                            empl_id = u.empl_id,
+                            source_id = u.source_id
+                    FROM operate.report_items s
+                        INNER JOIN temp_items_table_update u ON u.id = s.id
+                    WHERE EXISTS (
+                        SELECT s.item_name, s.order_count, s.empl_id, s.source_id
+                        EXCEPT
+                        SELECT u.item_name, u.order_count, u.empl_id, u.source_id
+                        ) and t.id = s.id;
+
+                    INSERT INTO operate.report_items (item_name, order_count, empl_id, source_id)
+                    SELECT     
+                        item_name, 
+                        order_count, 
+                        empl_id, 
+                        source_id
+                    FROM temp_items_table_update
+                    WHERE
+                        id is NULL;
+                    """)
+
+            connection.commit()
+        except Exception as ex:
+          connection.rollback()
+          raise ex
+
+
+def put_report_items_setings(datastrings: list, source_id: str):
+
+    if len(datastrings) > 0:
+        cursor = connection.cursor()
+
+        try:
+          
+          list_of_args = []
+          for newrow in datastrings:
+              
+                        # rs.source_id,
+                        # so.source_name,  
+                        # rs.report_item_id,
+                        # ri.item_name,
+                        # rs.view_permission
+
+              if (len(str(newrow[0])) == 0 
+                  and len(str(newrow[2])) == 0 
+                  and len(str(newrow[4])) == 0):
+                  continue
+              
+              list_of_args.append("({}, '{}', {}, {}, {})"
+                  .format(num_to_query_substr(newrow[0]), #source_id
+                  newrow[2],                              #report_item_id
+                  num_to_query_substr(newrow[4])))        #view_permission
+                  
+
+          if len(list_of_args) > 0:
+            cursor.execute("""DROP TABLE IF EXISTS temp_setings_table_update""")
+            cursor.execute("""CREATE TEMP TABLE temp_setings_table_update AS SELECT * FROM operate.report_items_setings WHERE false""")
+
+            args_str = ','.join(list_of_args)
+            cursor.execute("""INSERT INTO temp_setings_table_update
+                                (source_id, report_item_id, view_permission)
+                              VALUES """ + args_str)
+            
+
+            cursor.execute("""
+                    
+                    UPDATE operate.report_items_setings t
+                        SET view_permission = u.view_permission
+                    FROM operate.report_items_setings s
+                        INNER JOIN temp_setings_table_update u ON u.source_id = s.source_id AND u.report_item_id = s.report_item_id
+                    WHERE EXISTS (
+                        SELECT s.view_permission
+                        EXCEPT
+                        SELECT u.view_permission
+                        ) and t.id = s.id;
+                    """)
+
+            connection.commit()
+        except Exception as ex:
+          connection.rollback()
+          raise ex
+
 
 @app.get("/dict_operate")
 def get_dict() -> dict:
@@ -227,16 +482,16 @@ def put_dict() -> dict:
         put_sources(datastrings)
 
     if dict_name == 'hotels':
-        None
+        put_hotels(datastrings)
 
     if dict_name == 'employees':
-        None
+        put_employees(datastrings)
 
     if dict_name == 'report_items':
-        None
+        put_report_items(datastrings)
 
     if dict_name == 'report_items_setings':
-        None
+        put_report_items_setings(datastrings, source_id)
 
 def lambda_handler(event, context):
     print({'event': event, 'context': context})

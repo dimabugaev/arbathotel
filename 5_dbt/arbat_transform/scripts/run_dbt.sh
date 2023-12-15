eba674a4f3fc5ca466d9ba87f50dc0ca4ae9c59d
Dmitry Bugaev <bugaefff@gmail.com>
2023-12-15 15:35:17 +0300

    fix contener script

--- 5_dbt/arbat_transform/scripts/run_dbt.sh
+++ 5_dbt/arbat_transform/scripts/run_dbt.sh
@@ -1,15 +1,15 @@
 echo "Running dbt:"
 echo ""
-dbt deps --profiles-dir ./dbt --project-dir ./dbt
+dbt deps --project-dir .
 echo ""
-if [ "$DBT_TRANSFORM_MODE" == "booking-problems-mart-update" ]; then
+if [ "$DBT_TRANSFORM_MODE" = "booking-problems-mart-update" ]; then
     echo "Booking problems marts updating..."
     echo ""
-    dbt run -s "+mart_ufms_applications, +mart_booking_problem" --profiles-dir ./dbt --project-dir ./dbt
-elif [ "$DBT_TRANSFORM_MODE" == "full-update" ]; then
+    dbt run -s "+mart_ufms_applications, +mart_booking_problem" --project-dir .
+elif [ "$DBT_TRANSFORM_MODE" = "full-update" ]; then
     echo "All tables updating..."
     echo ""
-    dbt run --profiles-dir ./dbt --project-dir ./dbt    
+    dbt run --project-dir .    
 else
     echo "Do nothing..."    
 fi

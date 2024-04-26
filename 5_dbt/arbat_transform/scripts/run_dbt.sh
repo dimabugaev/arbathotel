918baf58cdb12beef184c5f72b9f93042b6c396b
Dmitry Bugaev <bugaefff@gmail.com>
2024-04-26 16:42:33 +0200

    new mart launch

--- 5_dbt/arbat_transform/scripts/run_dbt.sh
+++ 5_dbt/arbat_transform/scripts/run_dbt.sh
@@ -9,7 +9,7 @@ echo ""
 if [ "$DBT_TRANSFORM_MODE" = "booking-problems-mart-update" ]; then
     echo "Booking problems marts updating..."
     echo ""
-    dbt run -s "+mart_ufms_applications +mart_booking_problem +mart_hotels_escapes +mart_bank_payments" --project-dir .
+    dbt run -s "+mart_ufms_applications +mart_booking_problem +mart_hotels_escapes +mart_bank_payments +mart_bank_saldo" --project-dir .
 elif [ "$DBT_TRANSFORM_MODE" = "full-update" ]; then
     echo "All tables updating..."
     echo ""

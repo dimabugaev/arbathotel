04a68cf0e443388c6e85d4a82a1d25bd42371ff7
Dmitry Bugaev <bugaefff@gmail.com>
2024-07-21 01:59:16 +0200

    src eq dbt

---
+++ 5_dbt/arbat_transform/models/calc/calc_psb_payments_with_aq.sql
@@ -0,0 +1,142 @@
+with psb_strings as (
+    select * from {{ source('banks', 'psb_docs_rows') }}
+)
+,aq_qr_refund as (
+    select * from {{ ref('src_psb_acquiring_qr_refund') }}
+)
+,aq_qr as (
+    select * from {{ ref('src_psb_acquiring_qr') }}
+)
+,aq_term as (
+    select * from {{ ref('src_psb_acquiring_term') }}
+)
+,aq_all as (
+    select
+        id_aq,  
+        operation_type,
+        terminal_number,
+        order_number,
+        description,
+        operation_data,
+        false debit, 
+        operation_sum summa_rur,
+        bank_payment_id,
+        source_id
+    from 
+       aq_term
+    union all
+    select
+        id_aq,  
+        operation_type,
+        terminal_number,
+        order_number,
+        description,
+        operation_data,
+        true debit, 
+        commission summa_rur,
+        bank_payment_id,
+        source_id
+    from 
+       aq_term 
+    union all
+    select
+        id_aq,  
+        'payment_qr' operation_type,
+        terminal_number,
+        order_number,
+        description,
+        operation_data,
+        false debit, 
+        operation_sum summa_rur,
+        bank_payment_id,
+        source_id
+    from 
+       aq_qr
+    union all
+    select
+        id_aq,  
+        'commision_qr',
+        terminal_number,
+        order_number,
+        description,
+        operation_data,
+        true debit, 
+        commission summa_rur,
+        bank_payment_id,
+        source_id
+    from 
+       aq_qr
+    union all
+    select
+        id_aq,  
+        'refund_qr' operation_type,
+        terminal_number,
+        order_number,
+        description,
+        operation_data,
+        false debit, 
+        operation_sum summa_rur,
+        bank_payment_id,
+        source_id
+    from 
+       aq_qr_refund 
+)
+,distrib_paymants as (
+    coalesce(aq.id_aq || aq.operation_type, pdr.doc_id::text) doc_id,
+	pdr.source_id,
+	pdr.row_date
+	coalesce(aq.debit, pdr.debit) debit,
+	pdr.outer_account,
+	pdr.kb,
+	pdr.contragent_inn,
+	pdr.contragent,
+	pdr.description,
+	coalesce(aq.summa_rur, pdr.summa_rur) summa_rur
+    pdr.debit,
+    aq.terminal_number,
+    aq.order_number 
+from
+    psb_strings pdr left join aq_all aq on pdr.doc_id = aq.bank_payment_id and pdr.source_id = aq.source_id    
+)
+select
+	pdr.doc_id id,
+	pdr.source_id,
+	s.source_external_key as account_number,
+	s.source_type, -- bank internal code
+    s.source_name,
+	pdr.row_date date_doc,
+	pdr.row_date date_transaction,
+	not pdr.debit as income,
+	pdr.outer_account as contragent_account,
+	pdr.kb as contragent_bic,
+	'' contragent_bank,
+	pdr.contragent_inn as contragent_inn,
+	pdr.contragent as contragent,
+	pdr.description as payment_purpose,
+	pdr.summa_rur amount,
+	case 
+		when pdr.debit then -- расход
+			pdr.summa_rur
+		else -- приход
+			0	
+	end as out_summ,
+	case 
+		when pdr.debit then -- расход
+			0
+		else -- приход
+			pdr.summa_rur	
+	end as in_summ,
+	SUM(case 
+		when pdr.debit then -- расход
+			0
+		else -- приход
+			pdr.summa_rur	
+	end - case 
+		when pdr.debit then -- расход
+			pdr.summa_rur
+		else -- приход
+			0	
+	end) OVER (partition by pdr.source_id ORDER BY pdr.row_date, pdr.doc_id) + COALESCE(s.source_income_debt,0) AS total_debt
+from 
+	distrib_paymants pdr join
+	{{ ref('src_sources') }} s on pdr.source_id = s.source_id
---
+++ 5_dbt/arbat_transform/models/mart/mart_bank_payments_aq.sql
@@ -0,0 +1,77 @@
+{{
+  config(
+	materialized = 'table',
+	)
+}}
+
+with banks_payments as (
+    select
+        source_id,
+        id::text,
+        account_number,
+        source_type,
+        source_name,
+        date_transaction,
+        in_summ,
+        out_summ,
+        payment_purpose,
+        contragent_inn,
+        contragent,
+        total_debt
+    from
+        {{ ref('src_bank_tinkoff_payments') }}
+    
+    union all
+
+    select
+        source_id,
+        id::text,
+        account_number,
+        source_type,
+        source_name,
+        date_transaction,
+        in_summ,
+        out_summ,
+        payment_purpose,
+        contragent_inn,
+        contragent,
+        total_debt
+    from
+        {{ ref('src_bank_psb_payments') }}
+
+    union all
+
+    select
+        source_id,
+        id::text,
+        account_number,
+        source_type,
+        source_name,
+        date_transaction,
+        in_summ,
+        out_summ,
+        payment_purpose,
+        contragent_inn,
+        contragent,
+        total_debt
+    from
+        {{ ref('src_bank_alfa_payments') }}
+)
+select
+    bp.source_id,
+    bp.id,
+    bp.account_number,
+    bp.source_name,
+    st.type_id,
+    st.type_name,
+    bp.date_transaction,
+    bp.in_summ,
+    bp.out_summ,
+    bp.payment_purpose,
+    bp.contragent_inn,
+    bp.contragent,
+    bp.total_debt,
+    ROW_NUMBER() OVER (ORDER BY bp.source_id, bp.date_transaction, bp.id) as sort_as_count_debt
+from
+   banks_payments bp join {{ ref('seed_sources_type_id') }} st
+   on bp.source_type = st.type_id

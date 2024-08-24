5d37b477893b8b598204b2b0656d69c5e167f023
Dmitry Bugaev <bugaefff@gmail.com>
2024-08-24 18:05:42 +0200

    new lambda extract invoices 2

--- 2_infrastructure/tf_modules/base_environment/lambdas_bnovo_extract.tf
+++ 2_infrastructure/tf_modules/base_environment/lambdas_bnovo_extract.tf
@@ -64,7 +64,7 @@ module "lambda_function_bnovo_invoices_extract" {
 
   publish = true
 
-  timeout = 120
+  timeout = 500
 
   create_package         = false
   local_existing_package = "${var.buildpath}${var.extract_bnovo_invoices_zip}"

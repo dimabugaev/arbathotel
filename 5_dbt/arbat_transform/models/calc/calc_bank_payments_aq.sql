da8ffebaf9219b3d6504e5670b497dfca6d088e1
Dmitry Bugaev <bugaefff@gmail.com>
2024-11-11 13:37:12 +0100

    synonyms

--- 5_dbt/arbat_transform/models/calc/calc_bank_payments_aq.sql
+++ 5_dbt/arbat_transform/models/calc/calc_bank_payments_aq.sql
@@ -22,7 +22,9 @@ with banks_payments as (
         null hotel_id,
         '' hotel_name,
         null terminal_number,
-        null order_number
+        null order_number,
+        null booking_id,
+        null booking_number
     from
         {{ ref('src_bank_tinkoff_payments') }}
     
@@ -45,7 +47,9 @@ with banks_payments as (
         hotel_id,
         hotel_name,
         terminal_number,
-        order_number
+        order_number,
+        booking_id,
+        booking_number
     from
         {{ ref('calc_psb_payments_with_aq') }}
 
@@ -68,9 +72,41 @@ with banks_payments as (
         null hotel_id,
         '' hotel_name,
         null terminal_number,
-        null order_number
+        null order_number,
+        null booking_id,
+        null booking_number
     from
         {{ ref('src_bank_alfa_payments') }}
+)
+,hotel_syn as (
+    select 
+        t1.id AS hotel_id,
+        t1.hotel_name,
+        trim(synonym) AS synonym
+    from {{ source('operate', 'hotels') }} t1,
+        LATERAL unnest(string_to_array(t1.synonyms, ',')) AS synonym
+)
+,out_payment_hotel_info as (
+    select
+        t1.source_id,
+        t1.id,
+        t1.id_aq,
+        t1.hotel_id,
+        t2.hotel_name
+    from 
+        (select
+            bp.source_id,
+            bp.id,
+            bp.id_aq,
+            min(hs.hotel_id) hotel_id
+        from 
+            banks_payments bp join hotel_syn hs on bp.payment_purpose like '%' || hs.synonym || '%'
+        where
+            bp.hotel_id is null and bp.out_summ <> 0 and bp.id_aq = ''
+        group by
+            bp.source_id,
+            bp.id,
+            bp.id_aq) as t1 join {{ source('operate', 'hotels') }} t2 on t1.hotel_id = t2.id    
 )
 select
     bp.source_id,
@@ -87,11 +123,14 @@ select
     bp.contragent_inn,
     bp.contragent,
     bp.total_debt,
-    bp.hotel_id hotel_id,
-    bp.hotel_name hotel_name,
+    coalesce(hi.hotel_id, bp.hotel_id) hotel_id,
+    coalesce(hi.hotel_name, bp.hotel_name) hotel_name,
     bp.terminal_number,
     bp.order_number,
+    bp.booking_id,
+    bp.booking_number,
     ROW_NUMBER() OVER (ORDER BY bp.source_id, bp.date_transaction, bp.id, bp.id_aq, bp.out_summ) as sort_as_count_debt
 from
    banks_payments bp join {{ ref('seed_sources_type_id') }} st
-   on bp.source_type = st.type_id
+   on bp.source_type = st.type_id 
+   left join out_payment_hotel_info hi on bp.source_id = hi.source_id and bp.id = hi.id and bp.id_aq = hi.id_aq
--- 5_dbt/arbat_transform/models/mart/mart_bank_payments_aq.sql
+++ 5_dbt/arbat_transform/models/mart/mart_bank_payments_aq.sql
@@ -4,102 +4,26 @@
 	)
 }}
 
-with banks_payments as (
-    select
-        source_id,
-        id::text,
-        '' id_aq,
-        account_number,
-        source_type,
-        source_name,
-        date_transaction,
-        in_summ,
-        out_summ,
-        payment_purpose,
-        contragent_inn,
-        contragent,
-        total_debt,
-        null hotel_id,
-        '' hotel_name,
-        null terminal_number,
-        null order_number,
-        null booking_id,
-        null booking_number
-    from
-        {{ ref('src_bank_tinkoff_payments') }}
-    
-    union all
-
-    select
-        source_id,
-        id::text,
-        id_aq,
-        account_number,
-        source_type,
-        source_name,
-        date_transaction,
-        in_summ,
-        out_summ,
-        payment_purpose,
-        contragent_inn,
-        contragent,
-        total_debt,
-        hotel_id,
-        hotel_name,
-        terminal_number,
-        order_number,
-        booking_id,
-        booking_number
-    from
-        {{ ref('calc_psb_payments_with_aq') }}
-
-    union all
-
-    select
-        source_id,
-        id::text,
-        '',
-        account_number,
-        source_type,
-        source_name,
-        date_transaction,
-        in_summ,
-        out_summ,
-        payment_purpose,
-        contragent_inn,
-        contragent,
-        total_debt,
-        null hotel_id,
-        '' hotel_name,
-        null terminal_number,
-        null order_number,
-        null booking_id,
-        null booking_number
-    from
-        {{ ref('src_bank_alfa_payments') }}
-)
 select
-    bp.source_id,
-    bp.id,
-    bp.id_aq,
-    bp.account_number,
-    bp.source_name,
-    st.type_id,
-    st.type_name,
-    bp.date_transaction,
-    bp.in_summ,
-    bp.out_summ,
-    bp.payment_purpose,
-    bp.contragent_inn,
-    bp.contragent,
-    bp.total_debt,
-    bp.hotel_id hotel_id,
-    bp.hotel_name hotel_name,
-    bp.terminal_number,
-    bp.order_number,
-    bp.booking_id,
-    bp.booking_number,
-    ROW_NUMBER() OVER (ORDER BY bp.source_id, bp.date_transaction, bp.id, bp.id_aq, bp.out_summ) as sort_as_count_debt
-from
-   banks_payments bp join {{ ref('seed_sources_type_id') }} st
-   on bp.source_type = st.type_id
+    source_id,
+    id,
+    id_aq,
+    account_number,
+    source_name,
+    type_id,
+    type_name,
+    date_transaction,
+    in_summ,
+    out_summ,
+    payment_purpose,
+    contragent_inn,
+    contragent,
+    total_debt,
+    hotel_id hotel_id,
+    hotel_name hotel_name,
+    terminal_number,
+    order_number,
+    booking_id,
+    booking_number,
+    sort_as_count_debt
+from {{ ref('calc_bank_payments_aq') }}

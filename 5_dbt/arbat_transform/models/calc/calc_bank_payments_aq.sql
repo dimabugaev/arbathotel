964e776577ffad3fc6f3399d5df7ec0e7a79a28c
Dmitry Bugaev <bugaefff@gmail.com>
2024-11-11 15:43:39 +0100

    invoices data

--- 5_dbt/arbat_transform/models/calc/calc_bank_payments_aq.sql
+++ 5_dbt/arbat_transform/models/calc/calc_bank_payments_aq.sql
@@ -108,6 +108,52 @@ with banks_payments as (
             bp.id,
             bp.id_aq) as t1 join {{ source('operate', 'hotels') }} t2 on t1.hotel_id = t2.id    
 )
+,bnovo_bank_invoices as (
+    select distinct
+        source_id,
+        cyrillic_invoice_number,
+        booking_id
+    from
+        {{ ref('src_bnovo_invoices_for_bank') }}
+)
+,booking_data_ordinary_in_payment as (
+    select
+        bp.source_id,
+        bp.id,
+        bp.id_aq,
+        min(bi.booking_id) booking_id
+    from 
+        (
+            select
+                source_id,
+                id,
+                id_aq,
+                unnest(regexp_matches(payment_purpose, '[а-яА-Яa-zA-Z]{2,3}-\d+', 'g')) as key_invoice 
+            from banks_payments
+            where
+                hotel_id is null and in_summ <> 0 and id_aq = ''
+        ) bp join bnovo_bank_invoices bi 
+            on bi.cyrillic_invoice_number = {{ convert_to_cyrillic('upper(bp.key_invoice)') }}
+                and bi.source_id = bp.source_id
+    group by
+        bp.source_id,
+        bp.id,
+        bp.id_aq
+)
+,ordinar_in_payment_hotel_info as (
+    select distinct
+        t1.source_id,
+        t1.id,
+        t1.id_aq,
+        t1.booking_id,
+        t2.booking_number,
+        t2.hotel_id,
+        t3.hotel_name
+    from 
+        booking_data_ordinary_in_payment as t1 
+        join {{ ref('src_bookings') }} t2 on t1.booking_id = t2.booking_id    
+        join {{ source('operate', 'hotels') }} t3 on t2.hotel_id = t3.id    
+)
 select
     bp.source_id,
     bp.id,
@@ -123,14 +169,15 @@ select
     bp.contragent_inn,
     bp.contragent,
     bp.total_debt,
-    coalesce(hi.hotel_id, bp.hotel_id) hotel_id,
-    coalesce(hi.hotel_name, bp.hotel_name) hotel_name,
+    coalesce(hi2.hotel_id, hi.hotel_id, bp.hotel_id) hotel_id,
+    coalesce(hi2.hotel_name, hi.hotel_name, bp.hotel_name) hotel_name,
     bp.terminal_number,
     bp.order_number,
-    bp.booking_id,
-    bp.booking_number,
+    coalesce(hi2.booking_id, bp.booking_id) booking_id,
+    coalesce(hi2.booking_id, bp.booking_number) booking_number,
     ROW_NUMBER() OVER (ORDER BY bp.source_id, bp.date_transaction, bp.id, bp.id_aq, bp.out_summ) as sort_as_count_debt
 from
    banks_payments bp join {{ ref('seed_sources_type_id') }} st
    on bp.source_type = st.type_id 
    left join out_payment_hotel_info hi on bp.source_id = hi.source_id and bp.id = hi.id and bp.id_aq = hi.id_aq
+   left join ordinar_in_payment_hotel_info hi2 on bp.source_id = hi2.source_id and bp.id = hi2.id and bp.id_aq = hi2.id_aq

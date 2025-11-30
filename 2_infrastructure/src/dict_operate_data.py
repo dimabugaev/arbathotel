fc8b53283b7e061ea7041ec1cc66ab7cc2db59bb
Dmitry Bugaev <bugaefff@gmail.com>
2025-11-30 13:33:14 +0100

    names in contragents

--- 2_infrastructure/src/dict_operate_data.py
+++ 2_infrastructure/src/dict_operate_data.py
@@ -335,9 +335,14 @@ def get_contragents() -> list:
                         co.account_number,
                         co.income_budget_item_id,
                         co.outcome_budget_item_id,
-                        co.hotel_id
+                        co.hotel_id,
+                        bi.item_name as income_budget_item_name,
+                        bo.item_name as outcome_budget_item_name,
+                        h.hotel_name
                       from 
-                        operate.contragents co
+                        operate.contragents co left join operate.budget_items bi on co.income_budget_item_id = bi.id
+                        left join operate.budget_items bo on co.outcome_budget_item_id = bo.id
+                        left join operate.hotels h on co.hotel_id = h.id
                       order by
                         co.id""")
     

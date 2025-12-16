a0375ace0056688241db86c10e6c290487c981d4
Dmitry Bugaev <bugaefff@gmail.com>
2025-12-16 20:19:03 +0100

    fix lambda layer

--- 2_infrastructure/tf_common/lambda.tf
+++ 2_infrastructure/tf_common/lambda.tf
@@ -1,9 +1,18 @@
 module "lambda_layer_common" {
   source = "terraform-aws-modules/lambda/aws"
 
+  create_function = false
   create_layer = true
   layer_name          = "lambda-layer-common-lib"
-  description         = "lambda-layer-common-lib"
+  description         = "common library for lambda functions"
   compatible_runtimes = ["python3.10"]
-  source_path = "./../src/layer_common"
+  runtime             = "python3.10"
+  source_path = [
+    {
+      path             = "./../src/layer_common"
+      pip_requirements = true
+      prefix_in_zip    = "python" # required to get the path correct
+    }
+  ]
+
 }
